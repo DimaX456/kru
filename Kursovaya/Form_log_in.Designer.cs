21a0e827a2ac38895fe2028c7395094c9f0f4d86
DimaX456 <106971904+DimaX456@users.noreply.github.com>
2023-05-11 08:22:10 +0300

    Add files via upload

---
+++ Kursovaya/Form_log_in.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Kursovaya
+{
+    partial class Form_log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPassword1 = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.linkLabelReg = new System.Windows.Forms.LinkLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxPassword1
+            // 
+            this.textBoxPassword1.Location = new System.Drawing.Point(53, 185);
+            this.textBoxPassword1.Name = "textBoxPassword1";
+            this.textBoxPassword1.Size = new System.Drawing.Size(343, 22);
+            this.textBoxPassword1.TabIndex = 7;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(53, 111);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(343, 22);
+            this.textBoxUsername.TabIndex = 6;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(93, 275);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(284, 65);
+            this.buttonLogin.TabIndex = 5;
+            this.buttonLogin.Text = "log";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // linkLabelReg
+            // 
+            this.linkLabelReg.AutoSize = true;
+            this.linkLabelReg.Location = new System.Drawing.Point(205, 392);
+            this.linkLabelReg.Name = "linkLabelReg";
+            this.linkLabelReg.Size = new System.Drawing.Size(27, 16);
+            this.linkLabelReg.TabIndex = 10;
+            this.linkLabelReg.TabStop = true;
+            this.linkLabelReg.Text = "reg";
+            this.linkLabelReg.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelReg_LinkClicked);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Kursovaya.Properties.Resources.kisspng_computer_icons_5af35a5f53fbd2_603305641525897823344;
+            this.pictureBox2.Location = new System.Drawing.Point(428, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Kursovaya.Properties.Resources.png_transparent_computer_icons_icon_design_eye_visualization_eyeball_logo_media_black;
+            this.pictureBox1.Location = new System.Drawing.Point(428, 172);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(497, 35);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 11;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form_log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.linkLabelReg);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxPassword1);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "Form_log_in";
+            this.Text = "Form_log_in";
+            this.Load += new System.EventHandler(this.Form_log_in_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxPassword1;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.LinkLabel linkLabelReg;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
---
+++ Kursovaya/Form_sign_up.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Kursovaya
+{
+    partial class Form_sign_up
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReg = new System.Windows.Forms.Button();
+            this.textBoxUsername1 = new System.Windows.Forms.TextBox();
+            this.textBoxPassword1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonReg
+            // 
+            this.buttonReg.Location = new System.Drawing.Point(89, 284);
+            this.buttonReg.Name = "buttonReg";
+            this.buttonReg.Size = new System.Drawing.Size(284, 65);
+            this.buttonReg.TabIndex = 0;
+            this.buttonReg.Text = "reg";
+            this.buttonReg.UseVisualStyleBackColor = true;
+            this.buttonReg.Click += new System.EventHandler(this.buttonReg_Click);
+            // 
+            // textBoxUsername1
+            // 
+            this.textBoxUsername1.Location = new System.Drawing.Point(49, 120);
+            this.textBoxUsername1.Name = "textBoxUsername1";
+            this.textBoxUsername1.Size = new System.Drawing.Size(343, 22);
+            this.textBoxUsername1.TabIndex = 1;
+            // 
+            // textBoxPassword1
+            // 
+            this.textBoxPassword1.Location = new System.Drawing.Point(49, 194);
+            this.textBoxPassword1.Name = "textBoxPassword1";
+            this.textBoxPassword1.Size = new System.Drawing.Size(343, 22);
+            this.textBoxPassword1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Kursovaya.Properties.Resources.kisspng_computer_icons_5af35a5f53fbd2_603305641525897823344;
+            this.pictureBox2.Location = new System.Drawing.Point(424, 181);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Kursovaya.Properties.Resources.png_transparent_computer_icons_icon_design_eye_visualization_eyeball_logo_media_black;
+            this.pictureBox1.Location = new System.Drawing.Point(424, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form_sign_up
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxPassword1);
+            this.Controls.Add(this.textBoxUsername1);
+            this.Controls.Add(this.buttonReg);
+            this.Name = "Form_sign_up";
+            this.Text = "Form_sign_up";
+            this.Load += new System.EventHandler(this.Form_sign_up_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReg;
+        private System.Windows.Forms.TextBox textBoxUsername1;
+        private System.Windows.Forms.TextBox textBoxPassword1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
